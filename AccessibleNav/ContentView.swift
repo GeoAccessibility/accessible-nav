d6f4014d8160ec634d39683b414a5435cbb447ec
han rodríguez <han@gonzalpi.com>
2023-07-09 15:06:32 -0700

    Use switch to alternate between views

--- AccessibleNav/ContentView.swift
+++ AccessibleNav/ContentView.swift
@@ -7,37 +7,57 @@
 
 import SwiftUI
 
+enum Pages {
+    case splash, signin, newTrip, filters, navigation
+}
+
 struct ContentView: View {
-    @State var isNavigating: Bool = false
+    @State var page: Pages = Pages.splash
     var body: some View {
-        if !isNavigating {
-            ZStack {
-                Image("Map")
-                    .resizable()
-                    .scaledToFill()
-                VStack {
-                    Spacer(minLength: 0.5 * UIScreen.main.bounds.height)
-                    LocationSelector()
-                        .frame(width: UIScreen.main.bounds.width)
-                        .shadow(radius: 10)
+        ZStack {
+            switch page {
+            case Pages.splash:
+                SplashView()
+            case Pages.signin:
+                SignInView()
+            case Pages.newTrip:
+                ZStack {
+                    Image("Map")
+                        .resizable()
+                        .scaledToFill()
+                    VStack {
+                        Spacer(minLength: 0.5 * UIScreen.main.bounds.height)
+                        LocationSelector()
+                            .frame(width: UIScreen.main.bounds.width)
+                            .shadow(radius: 10)
+                    }
+                }
+            case Pages.filters:
+                FilterSelectorView()
+            case Pages.navigation:
+                ZStack {
+                    Image("Map")
+                        .resizable()
+                        .scaledToFill()
+                    VStack {
+                        CurrentDirection()
+                            .frame(width: UIScreen.main.bounds.width)
+                            .shadow(radius: 10)
+                        Spacer(minLength: 0.7 * UIScreen.main.bounds.height)
+                    }
+                    VStack {
+                        Spacer(minLength: 0.6 * UIScreen.main.bounds.height)
+                        ETA()
+                            .frame(width: UIScreen.main.bounds.width)
+                            .shadow(radius: 10)
+                    }
                 }
             }
-        } else {
-            ZStack {
-                Image("Map")
-                    .resizable()
-                    .scaledToFill()
-                VStack {
-                    CurrentDirection()
-                        .frame(width: UIScreen.main.bounds.width)
-                        .shadow(radius: 10)
-                    Spacer(minLength: 0.7 * UIScreen.main.bounds.height)
-                }
-                VStack {
-                    Spacer(minLength: 0.6 * UIScreen.main.bounds.height)
-                    ETA()
-                        .frame(width: UIScreen.main.bounds.width)
-                        .shadow(radius: 10)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.page = Pages.newTrip
                 }
             }
         }
--- AccessibleNav/SplashView.swift
+++ AccessibleNav/SplashView.swift
@@ -8,46 +8,33 @@
 import SwiftUI
 
 struct SplashView: View {
-    @State var isActive: Bool = false
-
     var body: some View {
         ZStack {
-            if self.isActive {
-                ContentView()
-            } else {
-                ZStack {
-                    Image("BgSplash")
+            ZStack {
+                Image("BgSplash")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(
+                        width: 1.15 * UIScreen.main.bounds.width,
+                        height: 1.15 * UIScreen.main.bounds.height
+                    )
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                VStack {
+                    Image("AppLogo")
                         .resizable()
-                        .scaledToFill()
-                        .frame(
-                            width: 1.15 * UIScreen.main.bounds.width,
-                            height: 1.15 * UIScreen.main.bounds.height
-                        )
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
-                    VStack {
-                        Image("AppLogo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 200)
-                        Text("NavigAbility Solutions")
-                            .fontWeight(.heavy)
-                            .foregroundColor(.white)
-                            .font(.system(size: 30))
-                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
-                        Text("Making navigation accessible")
-                            .italic()
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .font(.system(size: 20))
-                        
-                    }
-                }
-            }
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                withAnimation {
-                    self.isActive = true
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                    Text("NavigAbility Solutions")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                    Text("Making navigation accessible")
+                        .italic()
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                    
                 }
             }
         }
