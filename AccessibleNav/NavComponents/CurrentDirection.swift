5e5f19afc354c084faa444b40b1f781c94d029ba
han rodríguez <han@gonzalpi.com>
2023-07-09 04:41:24 -0700

    Add Location Selector and Current Direction navigation components

---
+++ AccessibleNav/NavComponents/CurrentDirection.swift
@@ -0,0 +1,88 @@
+//
+//  CurrentDirection.swift
+//  AccessibleNav
+//
+//  Created by rody on 2023-07-09.
+//
+
+import SwiftUI
+
+struct CurrentDirection: View {
+    @State private var direction: String = "E 17th Ave"
+    @State private var details: String = "toward Victor St"
+    @State private var currentArrow: String = "⬆️"
+
+    var body: some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .fill(.gray)
+                    .cornerRadius(20)
+                HStack {
+                    Text(currentArrow)
+                        .font(.system(size: 40))
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                    VStack {
+                        HStack {
+                            Text(direction)
+                                .fontWeight(.regular)
+                                .foregroundColor(.white)
+                                .font(.system(size: 17))
+                            Spacer()
+                        }
+                        Spacer()
+                        VStack {
+                            HStack {
+                                Text(details)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color(red: 0.85, green: 0.85, blue: 0.85))
+                                    .font(.system(size: 17))
+                                Spacer()
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+            HStack {
+                NextDirection()
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+struct NextDirection: View {
+    @State private var nextArrow: String = "➡️"
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.black)
+                .frame(width: 100, height: 44)
+                .cornerRadius(20)
+            Text("Then \(nextArrow)")
+                .fontWeight(.regular)
+                .foregroundColor(.white)
+                .font(.system(size: 17))
+        }
+    }
+}
+
+struct CurrentDirection_Preview: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Rectangle()
+                .frame(
+                    width: .infinity,
+                    height: 0.05 * UIScreen.main.bounds.height
+                )
+            CurrentDirection()
+            Rectangle()
+                .frame(
+                    width: .infinity,
+                    height: 0.7 * UIScreen.main.bounds.height
+                )
+        }
+    }
+}
