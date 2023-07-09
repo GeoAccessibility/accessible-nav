587770f22791fbec12ff83e469be85a8af03baf2
han rodríguez <han@gonzalpi.com>
2023-07-08 19:44:54 -0700

    Initial Swift app setup

---
+++ AccessibleNav/AccessibleNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibleNavApp.swift
+//  AccessibleNav
+//
+//  Created by rody on 2023-07-08.
+//
+
+import SwiftUI
+
+@main
+struct AccessibleNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
