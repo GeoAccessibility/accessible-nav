c6cdba87abbf395daaf61e19ee8f4ebbe89f978d
han rodríguez <han@gonzalpi.com>
2023-07-08 22:35:27 -0700

    Add SignInView

---
+++ AccessibleNav/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  AccessibleNav
+//
+//  Created by rody on 2023-07-08.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("Sign In")
+    }
+}
+
+struct SignInViewPreview: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
